812e9dd9d48dfb2e1d850751585fc53a2fedc485
Annihilator544 <94665572+Annihilator544@users.noreply.github.com>
2023-08-08 11:31:14 +0530

    Add files via upload

---
+++ Year 3/Compiler Design/Lab_02_Exercise_1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+    FILE *fptr1, *fptr2;
+    char filename[100], c;
+    int count = 0;
+    int count1 = 0;
+    // printf("Enter the filename to open for reading \n");
+    // scanf("%s", filename);
+    fptr1 = fopen("Lab_01_Exercise_1.c", "r");
+    if (fptr1 == NULL){
+        printf("Cannot open file %s \n", filename);
+        exit(0);
+    }
+    for (c = getc(fptr1); c != EOF; c = getc(fptr1)){
+        if (c == '\n'||c=='\t'){ // Increment count if this character is newline
+            count1 = count1 + 1;
+            c=' ';
+        }
+        printf("%c",c);
+        count++;
+    }    
+    printf("number of characters = %d\n", count);
+    printf("number of lines = %d\n", count1);
+    fclose(fptr1);
+    return 0;
+}
