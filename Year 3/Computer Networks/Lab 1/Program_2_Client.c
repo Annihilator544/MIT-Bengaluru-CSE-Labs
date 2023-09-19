ea9881be47d01d6f3289871314a76c46d7503463
Annihilator544 <94665572+Annihilator544@users.noreply.github.com>
2023-09-19 11:07:38 +0530

    Finally Made short asf programs

---
+++ Year 3/Computer Networks/Lab 1/Program_2_Client.c
@@ -0,0 +1,39 @@
+//Make the necessary includes and set up the variables
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<stdio.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+#include<stdlib.h>
+#include<string.h>
+#include <unistd.h>
+int main()
+{
+int len,result,sockfd,n=1;
+struct sockaddr_in address;
+char ch[256],buf[256];
+//Create a socket for the client
+sockfd = socket(AF_INET, SOCK_STREAM, 0);//Name the socket as agreed with the server
+address.sin_family=AF_INET;
+address.sin_addr.s_addr=inet_addr("172.20.16.37");
+address.sin_port=htons(10200);
+len = sizeof(address);
+//Connect your socket to the server’s socket
+result=connect(sockfd,(struct sockaddr *)&address,len);
+if(result==-1)
+{
+perror("\nCLIENT ERROR");
+exit(1);
+}
+//You can now read and write via sockfd (Logic for problem mentioned here)
+printf("\nENTER STRING\t");
+fgets(ch,256,stdin);
+ch[strlen(ch)]='\0';
+write(sockfd,ch,strlen(ch));//write the string to the server
+printf("STRING SENT BACK FROM SERVER IS ..... ");
+while(n){
+n=read(sockfd,buf,sizeof(buf));//read the string from the server
+puts(buf);//print the string
+}
+}
+
---
+++ Year 3/Computer Networks/Lab 1/Program_2_Server.c
@@ -0,0 +1,56 @@
+// Make the necessary includes and set up the variables:
+#include<stdio.h>
+#include<string.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#define PORTNO 10200
+int main()
+{
+int sockfd,newsockfd,portno,clilen,n=1;
+struct sockaddr_in seraddr,cliaddr;
+int i,value;
+// create an unnamed socket for the server
+sockfd = socket(AF_INET,SOCK_STREAM,0);
+//Name the socket
+seraddr.sin_family = AF_INET;
+seraddr.sin_addr.s_addr = inet_addr("172.20.16.37");// **
+seraddr.sin_port = htons(PORTNO);
+bind(sockfd,(struct sockaddr *)&seraddr,sizeof(seraddr));
+//Create a connection queue and wait for clients
+listen(sockfd,5);
+while (1) {
+char buf[256];
+printf("server waiting");
+//Accept a connection
+clilen = sizeof(clilen);
+newsockfd=accept(sockfd,(struct sockaddr *)&cliaddr,&clilen);
+//Read and write to client on client_sockfd (Logic for problem mentioned here)
+n = read(newsockfd,buf,sizeof(buf));//read the string from the client
+//remove duplicate words from string and store it in buf1
+char buf1[256];
+int k=0;
+for(int i=0;i<strlen(buf);i++)
+{
+if(buf[i]==' ')
+{
+buf1[k]=buf[i];
+k++;
+while(buf[i+1]==' ')
+{
+i++;
+}
+}
+else
+{
+buf1[k]=buf[i];
+k++;
+}
+}
+buf1[k]='\0';
+printf(" \nMessage from Client %s \n",buf);
+n = write(newsockfd,buf,sizeof(buf));//write the string to the client
+}
+}
