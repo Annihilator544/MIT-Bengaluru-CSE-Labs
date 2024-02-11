95123cd70eb4292e07dbf9b8907e9bfe876dcfd5
Annihilator544 <94665572+Annihilator544@users.noreply.github.com>
2024-02-12 01:59:00 +0530

    Can't sleep

---
+++ Year 3/Semester 6/Parallel Programming/Lab 1 (in c)/Exercise1.c
@@ -0,0 +1,26 @@
+#include<mpi.h>
+#include<stdio.h>
+#include<math.h>
+
+int main(int argc, char* argv[])
+{
+	int size, rank;
+	int x;
+
+
+	MPI_Init(&argc, &argv);
+	MPI_Comm_size(MPI_COMM_WORLD,&size);
+	MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+
+	printf("Enter x:\n");
+	scanf("%d",&x);
+
+	int ans = 1;
+	for(int i=1;i<=rank;i++){
+		ans=ans*x;
+	}
+
+	printf("Value of pow(%d,%d) from process %d is %d.\n",x,rank,rank,ans);
+
+	MPI_Finalize();
+}
