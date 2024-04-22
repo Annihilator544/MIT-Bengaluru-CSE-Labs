0d40231dc89b78222ce9153e211204259ebcc1ff
Aditya <adityaa0544@gmail.com>
2024-04-22 22:27:31 +0530

    django updates

--- Year 3/Semester 6/Internet Technologies Lab/Lab 7/Exercise2/locallibrary/settings.py
+++ Year 3/Semester 6/Internet Technologies Lab/Lab 7/Exercise2/locallibrary/settings.py
@@ -35,7 +35,7 @@ DEBUG = True
 # DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 # Set hosts to allow any app on Railway and the local testing URL
-ALLOWED_HOSTS = ['.railway.app', '.pythonanywhere.com', '127.0.0.1']
+ALLOWED_HOSTS = ['.railway.app', '.pythonanywhere.com', '127.0.0.1', 'localhost']
 
 # Set CSRF trusted origins to allow any app on Railway and the local testing URL
 CSRF_TRUSTED_ORIGINS = ['https://*.railway.app',
